260dcdadd8327880b22a6837cf99b90d6f0da972
WangberlinT <wangberlin2000@gmail.com>
2024-04-18 18:44:18 +0800

    testing for db implementation search all

--- app/src/androidTest/java/com/tiki/cryptoinfodemo/data/database/CurrencyInfoDaoTest.kt
+++ app/src/androidTest/java/com/tiki/cryptoinfodemo/data/database/CurrencyInfoDaoTest.kt
@@ -35,20 +35,6 @@ class CurrencyInfoDaoTest {
     }
 
     @Test
-    fun insertCurrencyInfoWithCryptoType() = runBlocking {
-        val currencyInfo = CurrencyInfo(
-            id = "1",
-            name = "Bitcoin",
-            symbol = "BTC",
-            code = null,
-            type = CurrencyType.CRYPTO
-        )
-        currencyInfoDao.insertAll(listOf(currencyInfo))
-        val allCurrencyInfo = currencyInfoDao.getAll()
-        Truth.assertThat(allCurrencyInfo[0].type).isEqualTo(CurrencyType.CRYPTO)
-    }
-
-    @Test
     fun insert10CurrencyInfoWith5FiatTypeAnd5CryptoType() = runBlocking {
         val fiat = (1..5).map {
             CurrencyInfo(
@@ -75,4 +61,121 @@ class CurrencyInfoDaoTest {
         Truth.assertThat(cryptoCount).isEqualTo(5)
     }
 
+    @Test
+    fun getAllCryptoCurrencies() = runBlocking {
+        val fiat = (1..5).map {
+            CurrencyInfo(
+                id = "Fiat$it",
+                name = "Dollar $it",
+                symbol = "F$it",
+                code = "F$it",
+                type = CurrencyType.FIAT
+            )
+        }
+        val crypto = (1..5).map {
+            CurrencyInfo(
+                id = "Crypto$it",
+                name = "Bitcoin $it",
+                symbol = "BTC$it",
+                code = "BTC$it",
+                type = CurrencyType.CRYPTO
+            )
+        }
+        currencyInfoDao.insertAll(fiat + crypto)
+        val allCryptoCurrencies = currencyInfoDao.getAllCryptoCurrencies()
+        Truth.assertThat(allCryptoCurrencies.size).isEqualTo(5)
+        Truth.assertThat(allCryptoCurrencies).isEqualTo(crypto)
+    }
+
+    @Test
+    fun getAllFiatCurrencies() = runBlocking {
+        val fiat = (1..5).map {
+            CurrencyInfo(
+                id = "Fiat$it",
+                name = "Dollar $it",
+                symbol = "F$it",
+                code = "F$it",
+                type = CurrencyType.FIAT
+            )
+        }
+        val crypto = (1..5).map {
+            CurrencyInfo(
+                id = "Crypto$it",
+                name = "Bitcoin $it",
+                symbol = "BTC$it",
+                code = "BTC$it",
+                type = CurrencyType.CRYPTO
+            )
+        }
+        currencyInfoDao.insertAll(fiat + crypto)
+        val allFiatCurrencies = currencyInfoDao.getAllFiatCurrencies()
+        Truth.assertThat(allFiatCurrencies).isEqualTo(fiat)
+    }
+
+    @Test
+    fun searchInAllCurrenciesCase1() = runBlocking {
+        val target = CurrencyInfo(
+            id = "Fiat1",
+            name = "Foobar",
+            symbol = "F1",
+            code = "F1",
+            type = CurrencyType.FIAT
+        )
+
+        val currencies = listOf(
+            target,
+            target.copy(id = "Fiat2", name = "BFoo"),
+            CurrencyInfo(
+                id = "Crypto1",
+                name = "Barfoo",
+                symbol = "BTC",
+                code = "BTC",
+                type = CurrencyType.CRYPTO
+            ),
+        )
+        currencyInfoDao.insertAll(currencies)
+        val searchResult = currencyInfoDao.search("Foo")
+        Truth.assertThat(searchResult).isEqualTo(listOf(target))
+    }
+
+    @Test
+    fun searchInAllCurrenciesCase2() = runBlocking {
+        val pattern = "Foo"
+        val target = listOf(
+            CurrencyInfo(
+                id = "Fiat1",
+                name = "Foobar",
+                symbol = "F1",
+                code = "F1",
+                type = CurrencyType.FIAT
+            ),
+            CurrencyInfo(
+                id = "Fiat2",
+                name = "BFoo",
+                symbol = "Foo2",
+                code = "F2",
+                type = CurrencyType.FIAT
+            ),
+            CurrencyInfo(
+                id = "Fiat3",
+                name = "Not Me",
+                symbol = "Not Me",
+                code = "Foo2",
+                type = CurrencyType.FIAT
+            ),
+        )
+
+        val currencies = target + listOf(
+            CurrencyInfo(
+                id = "Crypto1",
+                name = "Barfoo",
+                symbol = "BTC",
+                code = "BTC",
+                type = CurrencyType.CRYPTO
+            ),
+        )
+        currencyInfoDao.insertAll(currencies)
+        val searchResult = currencyInfoDao.search(pattern)
+        Truth.assertThat(searchResult).isEqualTo(target)
+    }
 }
--- app/src/main/java/com/tiki/cryptoinfodemo/data/database/CurrencyInfoDao.kt
+++ app/src/main/java/com/tiki/cryptoinfodemo/data/database/CurrencyInfoDao.kt
@@ -14,9 +14,9 @@ interface CurrencyInfoDao {
     @Query("SELECT * FROM currency_info WHERE type = 'CRYPTO'")
     suspend fun getAllCryptoCurrencies(): List<CurrencyInfo>
 
-    suspend fun getAllFiatCurrencies(): List<CurrencyInfo> {
-        TODO()
-    }
+    @Query("SELECT * FROM currency_info WHERE type = 'FIAT'")
+    suspend fun getAllFiatCurrencies(): List<CurrencyInfo>
+
 
     suspend fun searchInCryptoCurrencies(query: String): List<CurrencyInfo> {
         TODO()
@@ -26,9 +26,13 @@ interface CurrencyInfoDao {
         TODO()
     }
 
-    suspend fun search(query: String): List<CurrencyInfo> {
-        TODO()
-    }
+    @Query(
+        "SELECT * FROM currency_info " +
+                "WHERE name LIKE :pattern || '%' OR name LIKE '% ' || :pattern || '%'" +
+                "OR symbol LIKE :pattern || '%' OR symbol LIKE '% ' || :pattern || '%'" +
+                "OR code LIKE :pattern || '%' OR code LIKE '% ' || :pattern || '%'"
+    )
+    suspend fun search(pattern: String): List<CurrencyInfo>
 
 
     @Query("DELETE FROM currency_info")
